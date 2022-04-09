24185b0febfe84282638f7c6cb1d9a8a910ac5e5
Risel <roman.ignatowi413@gmail.com>
2022-04-09 21:46:45 +0300

    Added local storage connection
    I refactored code a liitle bit to connect all of task to local storage

--- script.js
+++ script.js
@@ -1,52 +1,47 @@
+let addBtn = document.getElementById("add-task-button");
+let taskField = document.getElementById("input-task");
+let delBtns = document.getElementsByClassName("delete-btn");
+let ul = document.getElementById("task-list");
 
-function addTask() {
-    let inputTask = document.getElementById("input-task");
-    let text = inputTask.value;
-    inputTask.value = "";
-    
-    let li = document.createElement("li");
-    li.className = "task-box";
-        
-    let checkBox = document.createElement('input')
-    checkBox.type = 'checkbox';
-    checkBox.className = "custom-checkbox";
-    checkBox.id = "checkbox"
-
-
-    let taskSpan = document.createElement("span");
-    taskSpan.className = "task";
-    taskSpan.textContent = text;
-
-    let buttonDel = document.createElement("button");
-    buttonDel.className = "delete-btn";
-    buttonDel.textContent = "Удалить задачу";
-    buttonDel.addEventListener('click', delTask);
-
-    li.append(checkBox, taskSpan, buttonDel);
-
-    let taskList = document.getElementById("task-list")
-    taskList.append(li);
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+addBtn.onclick = function() {
+    let taskName = taskField.value;
+    taskField.value = "";
+    taskList.push(taskName);
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+    showTask(taskName);
 }
 
+function showTask(taskName) {
+    if (taskName !== "") {
+        let li = document.createElement("li");
+        li.className ='task-box';
 
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.className = "custom-checkbox"
+        checkbox.id = "checkbox"
 
-function delTask(event) {
-    event.preventDefault();
-    let target = event.target;
-    let item = target.closest('li');
-    if (item) {
-        item.remove();
+        let span = document.createElement("span");
+        span.className = 'task';
+        span.textContent = taskName;
+
+        let delBut = document.createElement("button");
+        delBut.className = "delete-btn";
+        delBut.textContent = "Удалить задачу";
+
+        li.appendChild(checkbox);
+        li.appendChild(span);
+        li.appendChild(delBut);
+        ul.appendChild(li);
+
+        delBut.onclick = function () {
+            let li = this.parentNode;
+            let ul = li.parentNode;
+            ul.removeChild(li);
+
+            taskList = taskList.filter(e => e !== taskName);
+            localStorage.setItem("tasks", JSON.stringify(taskList));
+        }
     }
 }
-
-function init() {
-    let buttonAdd = document.getElementById("add-task-button");
-    buttonAdd.onclick = addTask;
-
-    const TASKS = document.querySelectorAll('.delete-btn');
-    TASKS.forEach((task) => {
-        task.addEventListener('click', delTask);
-    })
-}
-
-window.onload = init;
